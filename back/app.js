0ac88d053acac321c2cbed676b3ee8c4de19cdd0
doongle <qkrcksdna@naver.com>
2023-06-05 12:50:57 +0900

    [Node] Initiation

---
+++ back/app.js
@@ -0,0 +1,22 @@
+const express = require('express');
+const morgan = require('morgan');
+const path = require('path');
+const bodyParser = require('body-parser');
+const cookieParser = require('cookie-parser');
+const router = express.Router();
+const env = require('dotenv').config({ path: '../../.env' });
+
+const app = express();
+
+app.set('port', process.env.PORT || 8000);
+app.use(morgan('dev'));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(cookieParser());
+app.use(express.static(path.join(__dirname, 'public')));
+
+let main = require('./routes/main.js');
+app.use('/', main);
+app.listen(app.get('port'), () => {
+   console.log('8000 Port : Server Started...');
+});
